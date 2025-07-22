7df0b80ce0e446c59e940cd4f093b2244e136005
anonymmized <lih0deev@inbox.ru>
2025-07-22 19:35:57 +0300

    Add visual commands

---
+++ visual-commands.py
@@ -0,0 +1,9 @@
+def visual_commands_list():
+    visual_commnds_list = """
+    > - shift right
+    < - shift left
+    y - yank (copy) marked text
+    d - delete marked text
+    ~ - switch case
+    """
+    return visual_commnds_list
