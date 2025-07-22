dfc01d584610e073ac46b754ed307ef5cc0b0d26
anonymmized <lih0deev@inbox.ru>
2025-07-22 22:02:54 +0300

    Unnecessary files are deleted

--- exiting.py
+++
@@ -1,8 +0,0 @@
-def exiting_list():
-    ext_list = """ 
-    :w - write (save) the file, but don't exit
-    :wq - write (save) and quit
-    :q - quit (fails if anything has changed)
-    :q! - quit and throw away changes
-    """
-    return ext_list
--- visual_commands.py
+++
@@ -1,9 +0,0 @@
-def visual_commands_list():
-    visual_commnds_list = """
-    > - shift right
-    < - shift left
-    y - yank (copy) marked text
-    d - delete marked text
-    ~ - switch case
-    """
-    return visual_commnds_list
